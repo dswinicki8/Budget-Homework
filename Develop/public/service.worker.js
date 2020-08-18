b1fff104d5692d2bc39f3c0371c183c0f06a690e
Drew Swinicki <dswinicki93@gmail.com>
2020-08-18 18:05:54 -0400

    updated app.js for offline logic

--- Develop/public/service.worker.js
+++ Develop/public/service.worker.js
@@ -1,11 +1,12 @@
-const FILES_TO_CACHE = ["/", "/index.html", "index.js", "app.js", "style.css"];
+const FILES_TO_CACHE = ["/", "index.html", "index.js", "app.js", "style.css"];
 
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 
 // install
-self.addEventListener("install", function (evt) {
-  evt.waitUntil(
+self.addEventListener("install", (event) => {
+  console.log("service worker installed");
+  event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       console.log("Your files were pre-cached successfully!");
       return cache.addAll(FILES_TO_CACHE);
@@ -16,8 +17,8 @@ self.addEventListener("install", function (evt) {
 });
 
 // activate
-self.addEventListener("activate", function (evt) {
-  evt.waitUntil(
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
     caches.keys().then((keyList) => {
       return Promise.all(
         keyList.map((key) => {
@@ -34,24 +35,24 @@ self.addEventListener("activate", function (evt) {
 });
 
 // fetch
-self.addEventListener("fetch", function (evt) {
-  if (evt.request.url.includes("/api/")) {
-    evt.respondWith(
+self.addEventListener("fetch", (event) => {
+  if (event.request.url.includes("/api/")) {
+    event.respondWith(
       caches
         .open(DATA_CACHE_NAME)
         .then((cache) => {
-          return fetch(evt.request)
+          return fetch(event.request)
             .then((response) => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
-                cache.put(evt.request.url, response.clone());
+                cache.put(event.request.url, response.clone());
               }
 
               return response;
             })
             .catch((err) => {
               // Network request failed, try to get it from the cache.
-              return cache.match(evt.request);
+              return cache.match(event.request);
             });
         })
         .catch((err) => console.log(err))
